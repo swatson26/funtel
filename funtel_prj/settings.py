76cd8204ba0e6073848fc99ceb8c98e9e03a166b
watson <watson@orbitalinsight.com>
2023-05-26 10:57:49 -0600

    safety?

--- funtel_prj/settings.py
+++ funtel_prj/settings.py
@@ -22,10 +22,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)%@%v(_*7gp1mlvw5xqr8^^&8x1=!+i)y*95h3mwlqbse3#c_%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+
+SECRET_KEY = config("SECRET_KEY")
 
 ALLOWED_HOSTS = []
 
@@ -161,3 +162,6 @@ LOGGING = {
         'level': 'DEBUG',
     },
 }
+
+import django_heroku
+django_heroku.settings(locals())
