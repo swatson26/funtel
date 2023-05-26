bf2a5990e512077f386298bacc19c49925d1976d
watson <watson@orbitalinsight.com>
2023-05-26 11:49:02 -0600

    debug off

--- funtel_prj/settings.py
+++ funtel_prj/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 SECRET_KEY = config("SECRET_KEY")
 
