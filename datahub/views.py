cc02728d0ef1d470c0dac6535047a9d5d4b8c74e
watson <watson@orbitalinsight.com>
2023-05-31 13:15:15 -0600

    mime issue

--- datahub/views.py
+++ datahub/views.py
@@ -5,6 +5,23 @@ from .models import SnotelSite, SnotelData
 from datetime import datetime, timedelta
 from rest_framework import viewsets
 from .serializers import SnotelSiteSerializer, SnotelDataSerializer
+from django.views import View
+from django.http import HttpResponse, HttpResponseNotFound
+
+import os
+
+class Assets(View):
+
+    def get(self, _request, filename):
+        path = os.path.join(os.path.dirname(__file__), 'dist', filename)
+
+        if os.path.isfile(path):
+            with open(path, 'rb') as file:
+                return HttpResponse(file.read(), 
+                                    content_type='application/javascript')
+        else:
+            return HttpResponseNotFound()
+
 
 class AllStationsView(viewsets.ViewSet):
     def list(self, request):
