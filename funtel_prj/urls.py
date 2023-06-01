a562342e8c25049132620f888ffad1f4cc797c70
watson <watson@orbitalinsight.com>
2023-06-01 15:20:45 -0600

    idk

--- funtel_prj/urls.py
+++ funtel_prj/urls.py
@@ -1,27 +1,12 @@
-"""
-URL configuration for funtel_prj project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include, re_path
 from datahub.views import AllStationsView, StationView
 from rest_framework import routers
 from django.shortcuts import render
-from django.contrib.staticfiles.views import serve
-
+from django.views.static import serve 
+from django.conf import settings
 import logging
+import os
 logger = logging.getLogger('testlogger')
 
 
@@ -38,8 +23,9 @@ router.register(r'stations', AllStationsView, 'stations')
 router.register(r'station', StationView, 'station')
 
 urlpatterns = [
-     re_path(r'^static/(?P<path>.*)$', serve),
      path('admin/', admin.site.urls),
      path('api/', include(router.urls)),
-     re_path(r"^$", render_react)
+     re_path(r"^$", render_react),
+     re_path(r'^static/(?P<path>.*)$', serve,{'document_root': os.path.join(settings.BASE_DIR, 'build/static')}),
+     re_path(r'^static/(?P<path>.*)$', serve,{'document_root': os.path.join(settings.STATIC_ROOT)}),
      ]
